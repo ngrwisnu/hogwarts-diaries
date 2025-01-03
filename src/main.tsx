2de74f676068da47ba875402d9cf6b20b4fbdf21
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-01-03 20:58:10 +0700

    initial setup

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import About from "./pages/About.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
