3f34533eedd11d02a81195f69d4c61bf7dbc4fea
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-01-05 13:25:06 +0700

    add env file

--- src/loader/detailLoader.ts
+++ src/loader/detailLoader.ts
@@ -2,7 +2,9 @@ export async function detailLoader({ request, params }) {
   const id = params.id;
 
   try {
-    const response = await fetch(`http://localhost:3000/api/${id}`);
+    const response = await fetch(
+      `${import.meta.env.VITE_SOME_KEY || "http://localhost:3000"}/api/${id}`,
+    );
 
     const data = await response.json();
     return data;
